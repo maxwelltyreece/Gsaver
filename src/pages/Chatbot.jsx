4b1e045f84f36058b1c46c53fe5ed03243f14e6d
Oluwatosin Odubayo <psyoo1@exmail.nottingham.ac.uk>
2023-10-08 11:16:42 +0100

    code: carousel of sprites with save button

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -1,72 +1,92 @@
 import React, { useState } from 'react';
 
-const Chatbot = () => (
+const spriteImages = [
+  '/data/Sprites/sprite1.png',
+  '/data/Sprites/sprite2.png',
+  '/data/Sprites/sprite3.png',
+  '/data/Sprites/sprite4.png',
+  '/data/Sprites/sprite5.png',
+  '/data/Sprites/sprite6.png',
+];
+
+const ChatbotPage = () => {
+  const [currentSpriteIndex, setCurrentSpriteIndex] = useState(0);
+  const [isSaved, setIsSaved] = useState(false);
+  const selectedSprite = spriteImages[currentSpriteIndex];
+
+  const handleNextSprite = () => {
+    setCurrentSpriteIndex((prevIndex) => (prevIndex + 1) % spriteImages.length);
+  };
+
+  const handlePrevSprite = () => {
+    setCurrentSpriteIndex((prevIndex) =>
+      prevIndex === 0 ? spriteImages.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleSaveSprite = () => {
+    setIsSaved(true);
+  };
+
+  const spriteContainerStyle = {
+    width: '300px',
+    height: '300px',
+    margin: '0 auto',
+    overflow: 'hidden',
+    position: 'relative',
+  };
+
+  const spriteImageStyle = {
+    width: '100%',
+    height: 'auto',
+  };
+
+  const buttonStyle = {
+    fontSize: '24px',
+    backgroundColor: '#007bff',
+    color: '#fff',
+    border: 'none',
+    padding: '1px 20px',
+    margin: '0 10px',
+    cursor: 'pointer',
+  };
+
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
         <p>Chatbot</p>
     </div>
-);
-
-function CharacterCustomisation() {
-  // State variables for character customization
-  const [skinColor, setSkinColor] = useState("defaultSkinColor");
-  const [gender, setGender] = useState("defaultGender");
-  const [hairStyle, setHairStyle] = useState("defaultHairStyle"); // You need to add hairStyle state
-
-  // Event handlers for customization options
-  function handleSkinColorChange(newSkinColor) {
-    setSkinColor(newSkinColor);
-  }
-
-  function handleGenderChange(newGender) {
-    setGender(newGender);
-  }
-
-  function handleHairStyleChange(newHairStyle) {
-    setHairStyle(newHairStyle);
-  }
-
-  return (
-    <div>
-      <h2>Customize Your Character</h2>
-
-      {/* Skin Color */}
-      <label>Skin Color:</label>
-      <select value={skinColor} onChange={(e) => handleSkinColorChange(e.target.value)}>
-        <option value="defaultSkinColor">Default</option>
-        <option value="light">Light</option>
-        <option value="medium">Medium</option>
-        <option value="dark">Dark</option>
-      </select>
-
-      {/* Gender */}
-      <label>Gender:</label>
-      <input
-        type="radio"
-        value="male"
-        checked={gender === 'male'}
-        onChange={() => handleGenderChange('male')}
-      /> Male
-      <input
-        type="radio"
-        value="female"
-        checked={gender === 'female'}
-        onChange={() => handleGenderChange('female')}
-      /> Female
-
-      {/* Hair Style */}
-      <label>Hair Style:</label>
-      <select value={hairStyle} onChange={(e) => handleHairStyleChange(e.target.value)}>
-        <option value="defaultHairStyle">Default</option>
-        {/* Add more hair style options as needed */}
-      </select>
-
-      {/* Render your sprite using the selected customization options */}
-      <img
-        src={`src/data/Sprites/${skinColor}-${gender}-${hairStyle}.png`}
-        alt="Customized Character"
-      />
+      {isSaved ? (
+        <div>
+          <h2>Selected Sprite:</h2>
+          <div style={spriteContainerStyle}>
+            <img src={selectedSprite} alt="Sprite" style={spriteImageStyle} />
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div style={spriteContainerStyle}>
+            <img
+              src={selectedSprite}
+              alt="Choose your chatbot"
+              style={spriteImageStyle}
+            />
+          </div>
+          <div style={{ marginTop: '20px' }}>
+            <button style={buttonStyle} onClick={handlePrevSprite}>
+              &#8249;
+            </button>
+            <button style={buttonStyle} onClick={handleNextSprite}>
+              &#8250;
+            </button>
+            <button style={buttonStyle} onClick={handleSaveSprite}>
+              Save
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default Chatbot;
+export default ChatbotPage;
