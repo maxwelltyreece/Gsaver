f6eeb5d0ab40898e1eee8c141a032877cbc93f89
tobisalau <ts.tobisalau@gmail.com>
2023-10-08 19:01:05 +0100

    text to speech

--- src/pages/Tips.jsx
+++ src/pages/Tips.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
 import { transactions, contextMenuItems, ordersGrid, tips, tipsGrid,comps, } from '../data/dummy';
@@ -7,10 +7,11 @@ import { Header } from '../components';
 
 const Tips = () => {
   const editing = { allowDeleting: true, allowEditing: true };
+  
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      <GridComponent
+      <GridComponent 
         id="gridcomp"
         dataSource={tips}
         allowPaging
@@ -27,6 +28,7 @@ const Tips = () => {
         </ColumnsDirective>
         <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
       </GridComponent>
+      
     </div>
   );
 };
